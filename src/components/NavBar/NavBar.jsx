203b6ee0c63899741112c623b06ce1b13747a7b4
Shivaji Vichare <onboard.icu@gmail.com>
2023-10-05 03:41:28 +0000

    Make navbar mobile friendly

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,13 +1,9 @@
 import React, { useState, useContext, useEffect } from 'react';
 import { Link, useNavigate } from 'react-router-dom';
 import { RiMenu3Line, RiCloseLine } from 'react-icons/ri';
-// import logo from '../../assets/logo/svg_logo.svg';
 import sp_logo from '../../assets/logo/g_y_b.png';
 import aam_logo from '../../assets/logo/always_around_me.png';
 
-// import home_g_y_b from '../../assets/images/home_g.png';
-// import login_g_y_b from '../../assets/images/login_g.png';
-// import about_g_y_b from '../../assets/images/about_g.png';
 import '../../assets/fonts/fonts.css';
 import { ParameterContext } from '../../App';
 
@@ -20,16 +16,6 @@ const Navbar = ({userLoggedIn, resetUserEmail}) => {
   const navigate = useNavigate();
   const [toggleMenu, setToggleMenu] = useState(false);
 
-const handleLoginWithGoogle = () => {
-    const redirectUrlFromEnv = process.env.REACT_APP_OAUTH_REDIRECT_URL
-    const clientId = '201175894539-gte8nppbkqha8j0o40cqe7opmrsgmofo.apps.googleusercontent.com';
-    const redirectUri = encodeURIComponent(redirectUrlFromEnv);
-    const scopes = ['email', 'profile'];
-    const authUrl = `https://accounts.google.com/o/oauth2/v2/auth?client_id=${clientId}&redirect_uri=${redirectUri}&response_type=token&scope=${scopes.join(' ')}`;
-
-    window.location.href = authUrl;
-  };
-
   const { user } = useUser();
 
   console.log("User according to navbar : ", user);
@@ -40,20 +26,22 @@ const handleLoginWithGoogle = () => {
         <div className="gpt3__navbar-links_logo">
           <Link to="/Home"> <img src={aam_logo} alt="logo" /> </Link>
         </div>
-        <div className="gpt3__navbar-button">
-          {user ? <Link to="/ProfileHome"> <button type="button">Home</button> </Link> :
-          <Link to="/Home"> <button type="button">Home</button> </Link>}
-        </div>
-        <div className="gpt3__navbar-button">
-          {user ? <LogoutWithGoogle /> :
-          <LoginWithGoogle />}
-        </div> 
-        <div className="gpt3__navbar-button">
-          <Link to="/About">
-            <button type="button">
-              About
-            </button>
-          </Link>
+        <div className="gpt3__navbar-links_buttons">
+          <div className="gpt3__navbar-button">
+            {user ? <Link to="/ProfileHome"> <button type="button">Home</button> </Link> :
+            <Link to="/Home"> <button type="button">Home</button> </Link>}
+          </div>
+          <div className="gpt3__navbar-button">
+            {user ? <LogoutWithGoogle /> :
+            <LoginWithGoogle />}
+          </div> 
+          <div className="gpt3__navbar-button">
+            <Link to="/About">
+              <button type="button">
+                About
+              </button>
+            </Link>
+          </div>
         </div>
         <div className="gpt3__navbar-menu">
         {toggleMenu
@@ -62,13 +50,21 @@ const handleLoginWithGoogle = () => {
         {toggleMenu && (
         <div className="gpt3__navbar-menu_container scale-up-center">
           <div className="gpt3__navbar-menu_container-links">
-            <p><a href="#home">Home</a></p>
-            <p><a href="#dashboard">Login</a></p>
-            <p><a href="#dashboard">About</a></p>
-          </div>
-          <div className="gpt3__navbar-menu_container-links-sign">
-            <p>Sign in</p>
-            <button type="button">Sign up</button>
+            <div className="gpt3__navbar-button">
+              <p>{user ? <Link to="/ProfileHome"> <button type="button" onClick={() => setToggleMenu(false)}>Home</button> </Link> :
+              <Link to="/Home"> <button type="button" onClick={() => setToggleMenu(false)}>Home</button> </Link>}</p>
+            </div>
+            <div className="gpt3__navbar-button">
+              <p>{user ? <LogoutWithGoogle /> :
+              <LoginWithGoogle />}</p>
+            </div>
+            <div className="gpt3__navbar-button">
+              <p><Link to="/About">
+              <button type="button" onClick={() => setToggleMenu(false)}>
+                About
+              </button>
+              </Link></p>
+            </div>
           </div>
         </div>
         )}
