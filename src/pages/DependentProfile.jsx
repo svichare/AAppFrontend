5b61334cedf5e7bc7ea052be7a88dce421ae6d77
technologic808 <20627603+technologic808@users.noreply.github.com>
2023-10-28 05:00:29 +0530

    Update DependentProfile.jsx
    Add Material UI buttons with icon

--- src/pages/DependentProfile.jsx
+++ src/pages/DependentProfile.jsx
@@ -1,5 +1,5 @@
-import {React, useState, useContext, useEffect} from "react";
-import { useNavigate } from 'react-router-dom';
+import { React, useState, useContext, useEffect } from "react";
+import { Link, useNavigate } from 'react-router-dom';
 
 import "./DependentProfile.css"
 
@@ -10,13 +10,15 @@ import { ParameterContext } from '../App';
 import { API } from '@aws-amplify/api'
 import { getDependentDetails } from '../graphql/queries'
 import { useDependent } from '../components/DependentContext';
+import { Button } from "@mui/material";
+import { Chat, Edit, Person } from "@mui/icons-material";
 
 function isIterable(item) {
   return typeof item !== 'undefined' && typeof item[Symbol.iterator] === 'function';
 }
 
 async function get_dependent_details(dependent_string_id) {
-  try { 
+  try {
     console.log("Checking request  : " + dependent_string_id);
     const response = await API.graphql({
       query: getDependentDetails,
@@ -38,11 +40,11 @@ async function get_dependent_details(dependent_string_id) {
   } catch (error) {
     console.error(`Cought error in function : ${error}`);
     return {
-        "image_url": "https://upload.wikimedia.org/wikipedia/commons/thumb/3/3e/Google_2011_logo.png/320px-Google_2011_logo.png",
-        "name": "Error User",
-        "last_name": "SomeLastname",
-        "id": 1505
-      };
+      "image_url": "https://upload.wikimedia.org/wikipedia/commons/thumb/3/3e/Google_2011_logo.png/320px-Google_2011_logo.png",
+      "name": "Error User",
+      "last_name": "SomeLastname",
+      "id": 1505
+    };
   }
 }
 
@@ -58,12 +60,12 @@ export default function DependentProfile() {
     age: 4
   });
 
-  useEffect( () => {
+  useEffect(() => {
     if (dependent !== null) {
       get_dependent_details(dependent.string_id)
-      .then((profile_details_from_async) => {
-        setDependentData(profile_details_from_async);
-      });
+        .then((profile_details_from_async) => {
+          setDependentData(profile_details_from_async);
+        });
     } else {
       console.log("dependent data null. Heading home");
       navigate('/Home');
@@ -73,16 +75,16 @@ export default function DependentProfile() {
   const navigate = useNavigate();
 
   const navigateToPublicProf = () => {
-    navigate('/PublicProfile/' + dependentData.public_id );
+    navigate('/PublicProfile/' + dependentData.public_id);
   };
-  
+
   const navigateToVirtualAssistant = () => {
     navigate('/PublicProfile/' + dependentData.public_id + '/OpenAICaregiver');
   };
-  
+
   const returnProfilePic = () => {
     if (dependentData.name == null || typeof dependentData.name === 'undefined') {
-        return profile_pic_round;
+      return profile_pic_round;
     }
     if (dependentData.name == "Rishaan") {
       return rish_photo
@@ -90,11 +92,11 @@ export default function DependentProfile() {
 
     return profile_pic_round
   }
-  
+
   const [message, setMessage] = useState('Hover on the button to know more');
 
   const handleMouseEnter = (button) => {
-    console.log("Button details : " , button.target.textContent);
+    console.log("Button details : ", button.target.textContent);
     switch (button.target.textContent) {
       case 'Edit Care details':
         setMessage("Update details of how you take care of them. This information will always stay with them. ");
@@ -103,7 +105,7 @@ export default function DependentProfile() {
         setMessage("Default case");
         break;
     }
-    
+
   };
 
   const handleMouseLeave = () => {
@@ -113,7 +115,7 @@ export default function DependentProfile() {
 
   const returnProfileName = () => {
     if (dependentData.name === null || typeof dependentData.name === 'undefined') {
-        return  "";
+      return "";
     }
     if (dependentData.name == "Mock Value") {
       return "Loading..";
@@ -124,40 +126,41 @@ export default function DependentProfile() {
 
   const navButtons = [];
   if (dependentData.name === null ||
-      typeof dependentData.name === 'undefined' ||
-      dependentData.name == "Mock Value" ||
-      dependentData.name == "") {
+    typeof dependentData.name === 'undefined' ||
+    dependentData.name == "Mock Value" ||
+    dependentData.name == "") {
     // Dont populate the navbuttons here.
   } else {
     navButtons.push(
       <div className="DependentNavButtons">
-        <button type="button" onClick={() => {navigate('/TraitCategories')}} onMouseEnter={handleMouseEnter} onMouseLeave={handleMouseLeave}>Edit Care details</button>
-        <button type="button" onClick={() => {navigate('/UpdateDependentBio')}}>Edit Bio</button>
-        <button type="button" onClick={navigateToPublicProf}>See Public Profile</button> 
-        <button type="button" onClick={navigateToVirtualAssistant}>Chat with Virtual assistant</button>
+        <Button variant="contained" startIcon={<Edit />} component={Link} to="/TraitCategories" onMouseEnter={handleMouseEnter} onMouseLeave={handleMouseLeave}>Edit Care details</Button>
+        <Button variant="contained" startIcon={<Edit />} component={Link} to="/UpdateDependentBio">Edit Bio</Button>
+        <Button variant="contained" startIcon={<Person />} component={Link} to="/PublicProfile">See Public Profile</Button>
+        <Button variant="contained" startIcon={<Chat />} component={Link} to="/VirtualAssistant">Chat with Virtual assistant</Button>
       </div>
+
     );
   }
-  
+
   console.log("Showing dependent details for " + dependentStringId);
   return (
     <div className="DependentProfileContainer">
       <div className="DependentProfileMain">
         <div className="DependentHomeTopbar">
-            <div className="DependentHomeImage">
-              <img src={returnProfilePic()} alt="profile_photo" />
-            </div>
-            <div className="DependentHomeName">
-              <h3> {returnProfileName()} </h3>
-            </div>
+          <div className="DependentHomeImage">
+            <img src={returnProfilePic()} alt="profile_photo" />
+          </div>
+          <div className="DependentHomeName">
+            <h3> {returnProfileName()} </h3>
+          </div>
         </div>
         <div className="DependentProfileDescription">
-        
+
         </div>
         <p> Welcome to the Profile page of your dear one.</p>
         <p> Use the <b>Edit buttons</b> below to store the information you have about them.</p>
-        <p> View the <b>Public Profile</b> and share it with everyone important in their life. It is the page which has 
-         all the information needed to take care of them.</p>
+        <p> View the <b>Public Profile</b> and share it with everyone important in their life. It is the page which has
+          all the information needed to take care of them.</p>
         {navButtons}
       </div>
       <div className="Bottom">
