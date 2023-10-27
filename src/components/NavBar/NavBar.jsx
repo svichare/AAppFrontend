5e830185fd6d90b534af3e06e0cf9d1c8081b025
technologic808 <20627603+technologic808@users.noreply.github.com>
2023-10-28 04:12:46 +0530

    Update Navbar Buttons

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,29 +1,30 @@
 import React, { useState, useContext, useEffect, useRef } from 'react';
 import { Link, useNavigate } from 'react-router-dom';
 import { RiMenu3Line, RiCloseLine } from 'react-icons/ri';
-import sp_logo from '../../assets/logo/g_y_b.png';
 import aam_logo from '../../assets/logo/always_around_me.png';
 
-import '../../assets/fonts/fonts.css';
-import { ParameterContext } from '../../App';
+import Button from '@mui/material/Button';
 
-import {LoginWithGoogle, LogoutWithGoogle} from '../';
+import '../../assets/fonts/fonts.css';
+
+import { LoginWithGoogle, LogoutWithGoogle } from '../';
 import { useUser } from '../UserContext';
 
 import './NavBar.css';
+import { Home, Info, Login, Logout } from '@mui/icons-material';
 
-const Navbar = ({userLoggedIn, resetUserEmail}) => {
+const Navbar = ({ userLoggedIn, resetUserEmail }) => {
   const navigate = useNavigate();
   const [toggleMenu, setToggleMenu] = useState(false);
 
   const { user } = useUser();
 
-// Create a ref for the menu container
+  // Create a ref for the menu container
   const menuRef = useRef(null);
   const bodyRef = useRef(null);
   // Toggle the menu state when the icon is clicked
   const handleIconClick = () => {
-    console.log("Menu icon  clicked! "); 
+    console.log("Menu icon  clicked! ");
     setToggleMenu((prevToggle) => !prevToggle);
   };
   useEffect(() => {
@@ -57,47 +58,47 @@ const Navbar = ({userLoggedIn, resetUserEmail}) => {
           <Link to="/Home"> <img src={aam_logo} alt="logo" /> </Link>
         </div>
         <div className="gpt3__navbar-links_buttons">
-          <div className="gpt3__navbar-button">
+          <Button variant="contained" startIcon={<Home />} className="gpt3__navbar-button">
             {user ? <Link to="/ProfileHome"> <button type="button">Home</button> </Link> :
-            <Link to="/Home"> <button type="button">Home</button> </Link>}
-          </div>
-          <div className="gpt3__navbar-button">
+              <Link to="/Home"> <button type="button">Home</button> </Link>}
+          </Button>
+          <Button variant="contained" startIcon={user ? <Login /> : <Logout />} className="gpt3__navbar-button">
             {user ? <LogoutWithGoogle /> :
-            <LoginWithGoogle />}
-          </div> 
-          <div className="gpt3__navbar-button">
+              <LoginWithGoogle />}
+          </Button>
+          <Button variant="contained" startIcon={<Info />} s className="gpt3__navbar-button">
             <Link to="/About">
               <button type="button">
                 About
               </button>
             </Link>
-          </div>
+          </Button>
         </div>
         <div className="gpt3__navbar-menu" ref={menuRef}>
-        {toggleMenu
-          ? <RiCloseLine color="#fff" size={27} onClick={() => { setToggleMenu(false); }}/>
-          : <RiMenu3Line color="#fff" size={27} onClick={(e) => { e.stopPropagation(); setToggleMenu(true); }} />}
-        {toggleMenu && (
-        <div className="gpt3__navbar-menu_container scale-up-center">
-          <div className="gpt3__navbar-menu_container-links">
-            <div className="gpt3__navbar-button">
-              <p>{user ? <Link to="/ProfileHome"> <button type="button" onClick={() => setToggleMenu(false)}>Home</button> </Link> :
-              <Link to="/Home"> <button type="button" onClick={() => setToggleMenu(false)}>Home</button> </Link>}</p>
+          {toggleMenu
+            ? <RiCloseLine color="#fff" size={27} onClick={() => { setToggleMenu(false); }} />
+            : <RiMenu3Line color="#fff" size={27} onClick={(e) => { e.stopPropagation(); setToggleMenu(true); }} />}
+          {toggleMenu && (
+            <div className="gpt3__navbar-menu_container scale-up-center">
+              <div className="gpt3__navbar-menu_container-links">
+                <div className="gpt3__navbar-button">
+                  <p>{user ? <Link to="/ProfileHome"> <button type="button" onClick={() => setToggleMenu(false)}>Home</button> </Link> :
+                    <Link to="/Home"> <button type="button" onClick={() => setToggleMenu(false)}>Home</button> </Link>}</p>
+                </div>
+                <div className="gpt3__navbar-button">
+                  <p>{user ? <LogoutWithGoogle /> :
+                    <LoginWithGoogle />}</p>
+                </div>
+                <div className="gpt3__navbar-button">
+                  <p><Link to="/About">
+                    <button type="button" onClick={() => setToggleMenu(false)}>
+                      About
+                    </button>
+                  </Link></p>
+                </div>
+              </div>
             </div>
-            <div className="gpt3__navbar-button">
-              <p>{user ? <LogoutWithGoogle /> :
-              <LoginWithGoogle />}</p>
-            </div>
-            <div className="gpt3__navbar-button">
-              <p><Link to="/About">
-              <button type="button" onClick={() => setToggleMenu(false)}>
-                About
-              </button>
-              </Link></p>
-            </div>
-          </div>
-        </div>
-        )}
+          )}
         </div>
       </div>
     </div>
