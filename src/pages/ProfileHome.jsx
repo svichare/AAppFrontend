d1b9ce75e16396472a42d99b5b1e943dad3e05cc
Shivaji Vichare <onboard.icu@gmail.com>
2023-10-28 03:21:53 +0000

    UI changes

--- src/pages/ProfileHome.jsx
+++ src/pages/ProfileHome.jsx
@@ -206,8 +206,6 @@ export default function ProfileHome({userEmailParameter, resetUserEmail}) {
             </div>
   
             <p>Email : {localUserEmail}</p>
-            <h3> Welcome to your homepage.</h3>
-
             <div className="NextSteps">
               <p>{dependent_list.length <= 1 ? "Next steps: Add dependents to your profile": "To update or view profiles of your dependents, click on the list below"}</p>
             </div>
@@ -217,7 +215,7 @@ export default function ProfileHome({userEmailParameter, resetUserEmail}) {
             </div>
             <Link to="/UpdateProfile">
               <div className="UpdateProfileButton" onClick={()=>{}}>
-                <button type="button">Update Profile</button> 
+                <button type="button">Update Bio</button> 
               </div>
             </Link>
           </div>
