77052475e90116a0720d1ece3753e39c28c209dd
technologic808 <20627603+technologic808@users.noreply.github.com>
2023-10-29 19:00:47 +0530

    Fix Buttons on /UpdateDependentBio route

--- src/pages/profile_pages/UpdateDependentBio.jsx
+++ src/pages/profile_pages/UpdateDependentBio.jsx
@@ -1,6 +1,6 @@
-import {React, useEffect, useState, useContext} from "react";
+import { React, useEffect, useState, useContext } from "react";
 import { API, graphqlOperation } from '@aws-amplify/api'
-import { useNavigate } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 
 import { ParameterContext } from '../../App';
 
@@ -13,6 +13,7 @@ import { useUser } from '../../components/UserContext';
 import { useDependent } from '../../components/DependentContext';
 
 import './UpdateDependentBio.css'
+import { Button } from "@mui/material";
 
 function isIterable(item) {
   if (item === null) {
@@ -22,7 +23,7 @@ function isIterable(item) {
 }
 
 async function get_dependent_details(dependent_string_id) {
-  try { 
+  try {
     console.log("Checking request  : " + dependent_string_id);
     const response = await API.graphql({
       query: getDependentDetails,
@@ -44,11 +45,11 @@ async function get_dependent_details(dependent_string_id) {
   } catch (error) {
     console.error(`Cought error in function : ${error}`);
     return {
-        "image_url": "https://upload.wikimedia.org/wikipedia/commons/thumb/3/3e/Google_2011_logo.png/320px-Google_2011_logo.png",
-        "name": "Error User",
-        "last_name": "SomeLastname",
-        "id": 1505
-      };
+      "image_url": "https://upload.wikimedia.org/wikipedia/commons/thumb/3/3e/Google_2011_logo.png/320px-Google_2011_logo.png",
+      "name": "Error User",
+      "last_name": "SomeLastname",
+      "id": 1505
+    };
   }
 }
 
@@ -66,83 +67,83 @@ async function update_profile(profileDetails, parent_email_param) {
 
   try {
     console.log("Updating the response. with public id: ", JSON.stringify(update_dependent_details));
-      const response = await API.graphql(
-        graphqlOperation(UpdateDependentBio,
-          {updateDependentInput: update_dependent_details}));
-      console.log("received response : ", JSON.stringify(response));
+    const response = await API.graphql(
+      graphqlOperation(UpdateDependentBio,
+        { updateDependentInput: update_dependent_details }));
+    console.log("received response : ", JSON.stringify(response));
   } catch (error) {
-      console.error('Cought error in update_action_response function :',  JSON.stringify(error));
+    console.error('Cought error in update_action_response function :', JSON.stringify(error));
   }
 }
 
-export default function UpdateDependentBioPage({existingProfile}) {
-    const { userEmail } = useContext(ParameterContext);
-    const { user } = useUser();
-    const { dependent } = useDependent();
+export default function UpdateDependentBioPage({ existingProfile }) {
+  const { userEmail } = useContext(ParameterContext);
+  const { user } = useUser();
+  const { dependent } = useDependent();
 
-    const [localProfileDetails, setLocalProfileDetails] = useState({});
+  const [localProfileDetails, setLocalProfileDetails] = useState({});
 
-    const [userData, setUserData] = useState({
-        name: "",
-        last_name: "",
-        id: "topId",
-        image_url: "../assets/images/profile_picture.jpg",
-        dependents: []
-    });
-    const [dependentData, setDependentData] = useState({
-        name: "",
-        last_name: "",
-        id: "topId",
-        image_url: "../assets/images/profile_picture.jpg",
-        dependents: []
-    });
-    const [loading, setLoading] = useState(true);
+  const [userData, setUserData] = useState({
+    name: "",
+    last_name: "",
+    id: "topId",
+    image_url: "../assets/images/profile_picture.jpg",
+    dependents: []
+  });
+  const [dependentData, setDependentData] = useState({
+    name: "",
+    last_name: "",
+    id: "topId",
+    image_url: "../assets/images/profile_picture.jpg",
+    dependents: []
+  });
+  const [loading, setLoading] = useState(true);
 
-    useEffect( () => {
-        if (dependent === null) {
-          console.log("dependent data null. Returning.");
-          return;
-        }
-        if (user === null) {
-          console.log("user data null. Returning.");
-          return;
-        }
-        get_dependent_details(dependent.string_id)
-          .then((profile_details_from_async) => {
-            setDependentData(profile_details_from_async);
-            setLocalProfileDetails(profile_details_from_async);
-            setLoading(false);
-        });
+  useEffect(() => {
+    if (dependent === null) {
+      console.log("dependent data null. Returning.");
+      return;
+    }
+    if (user === null) {
+      console.log("user data null. Returning.");
+      return;
+    }
+    get_dependent_details(dependent.string_id)
+      .then((profile_details_from_async) => {
+        setDependentData(profile_details_from_async);
+        setLocalProfileDetails(profile_details_from_async);
+        setLoading(false);
+      });
   }, []);
 
-    const handleInputChange = (event) => {
-        const { name, value } = event.target;
-        console.log("Setting : ", name , " to value : ", value);
+  const handleInputChange = (event) => {
+    const { name, value } = event.target;
+    console.log("Setting : ", name, " to value : ", value);
 
-        setLocalProfileDetails({
-            ...localProfileDetails,
-            [name]: value,
-        });
-    };
+    setLocalProfileDetails({
+      ...localProfileDetails,
+      [name]: value,
+    });
+  };
 
-    const navigate = useNavigate();
+  const navigate = useNavigate();
 
-    const handleSubmit = async () => {
-      if (user === null) {
-        console.log("user null. Cannot update dependent");
-        return;
-      }
-      const result = await update_profile(localProfileDetails, user.email);
-      navigate('/DependentProfile');
-    };
-    
+  const handleSubmit = async () => {
+    if (user === null) {
+      console.log("user null. Cannot update dependent");
+      return;
+    }
+    const result = await update_profile(localProfileDetails, user.email);
+    navigate('/DependentProfile');
+  };
 
-    console.log("Latest dependentData ", JSON.stringify(dependentData));
-    console.log("Latest localProfileData ", JSON.stringify(localProfileDetails));
-    
-    return (
+
+  console.log("Latest dependentData ", JSON.stringify(dependentData));
+  console.log("Latest localProfileData ", JSON.stringify(localProfileDetails));
+
+  return (
     <div className="ProfileUpdateContainer">
-    
+
       <div className="ProfileUpdateMain">
         <h1>Details ...</h1>
         <p>(* required fields)</p>
@@ -150,13 +151,13 @@ export default function UpdateDependentBioPage({existingProfile}) {
           <div className="ProfileUpdateDetails">
             <p> First Name* </p>
             <input type="text"
-               defaultValue={(localProfileDetails === null ||
-                      typeof localProfileDetails.name ==='undefined' ) ? "":localProfileDetails.name} 
-               name="name"
-               value={(localProfileDetails === null ||
-                      typeof localProfileDetails.name ==='undefined' ) ?
-                      "": localProfileDetails.name}
-               onChange={handleInputChange}/>
+              defaultValue={(localProfileDetails === null ||
+                typeof localProfileDetails.name === 'undefined') ? "" : localProfileDetails.name}
+              name="name"
+              value={(localProfileDetails === null ||
+                typeof localProfileDetails.name === 'undefined') ?
+                "" : localProfileDetails.name}
+              onChange={handleInputChange} />
           </div>
         </div>
 
@@ -164,13 +165,13 @@ export default function UpdateDependentBioPage({existingProfile}) {
           <div className="ProfileUpdateDetails">
             <p> Diagnosis* </p>
             <input className="ProfileUpdateDetails" type="text"
-               defaultValue={(localProfileDetails === null ||
-                      typeof localProfileDetails.diagnosis ==='undefined' ) ? "":localProfileDetails.diagnosis} 
-               name="diagnosis"
-               value={(localProfileDetails === null ||
-                      typeof localProfileDetails.diagnosis ==='undefined' ) ?
-                      "": localProfileDetails.diagnosis}
-               onChange={handleInputChange}/>
+              defaultValue={(localProfileDetails === null ||
+                typeof localProfileDetails.diagnosis === 'undefined') ? "" : localProfileDetails.diagnosis}
+              name="diagnosis"
+              value={(localProfileDetails === null ||
+                typeof localProfileDetails.diagnosis === 'undefined') ?
+                "" : localProfileDetails.diagnosis}
+              onChange={handleInputChange} />
           </div>
         </div>
 
@@ -178,13 +179,13 @@ export default function UpdateDependentBioPage({existingProfile}) {
           <div className="ProfileUpdateDetails">
             <p> Communication type (eg. verbal/non-verbal)* </p>
             <input className="ProfileUpdateDetails" type="text"
-               defaultValue={(localProfileDetails === null ||
-                      typeof localProfileDetails.verbal ==='undefined' ) ? "":localProfileDetails.verbal} 
-               name="verbal"
-               value={(localProfileDetails === null ||
-                      typeof localProfileDetails.verbal ==='undefined' ) ?
-                      "": localProfileDetails.verbal}
-               onChange={handleInputChange}/>
+              defaultValue={(localProfileDetails === null ||
+                typeof localProfileDetails.verbal === 'undefined') ? "" : localProfileDetails.verbal}
+              name="verbal"
+              value={(localProfileDetails === null ||
+                typeof localProfileDetails.verbal === 'undefined') ?
+                "" : localProfileDetails.verbal}
+              onChange={handleInputChange} />
           </div>
         </div>
 
@@ -192,13 +193,13 @@ export default function UpdateDependentBioPage({existingProfile}) {
           <div className="ProfileUpdateDetails">
             <p> Age* </p>
             <input className="ProfileUpdateDetails" type="text"
-               defaultValue={(localProfileDetails === null ||
-                      typeof localProfileDetails.age ==='undefined' ) ? "":localProfileDetails.age} 
-               name="age"
-               value={(localProfileDetails === null ||
-                      typeof localProfileDetails.age ==='undefined' ) ?
-                      "": localProfileDetails.age}
-               onChange={handleInputChange}/>
+              defaultValue={(localProfileDetails === null ||
+                typeof localProfileDetails.age === 'undefined') ? "" : localProfileDetails.age}
+              name="age"
+              value={(localProfileDetails === null ||
+                typeof localProfileDetails.age === 'undefined') ?
+                "" : localProfileDetails.age}
+              onChange={handleInputChange} />
           </div>
         </div>
 
@@ -206,20 +207,26 @@ export default function UpdateDependentBioPage({existingProfile}) {
           <div className="ProfileUpdateDetails">
             <p> Public ID (to share the profile link with others)*</p>
             <input className="ProfileUpdateDetails" type="text"
-               defaultValue={(localProfileDetails === null ||
-                      typeof localProfileDetails.public_id ==='undefined' ) ? "":localProfileDetails.public_id} 
-               name="public_id"
-               value={(localProfileDetails === null ||
-                      typeof localProfileDetails.public_id ==='undefined' ) ?
-                      "": localProfileDetails.public_id}
-               onChange={handleInputChange}/>
+              defaultValue={(localProfileDetails === null ||
+                typeof localProfileDetails.public_id === 'undefined') ? "" : localProfileDetails.public_id}
+              name="public_id"
+              value={(localProfileDetails === null ||
+                typeof localProfileDetails.public_id === 'undefined') ?
+                "" : localProfileDetails.public_id}
+              onChange={handleInputChange} />
           </div>
         </div>
         <div className="ProfileUpdateItem">
-            <button type="button" onClick={handleSubmit}>Submit</button>
+          <Button variant="contained" size="large" color="primary" onClick={handleSubmit}>
+            Submit
+          </Button>
         </div>
         <div className="ProfileUpdateItem">
-            <button type="button" onClick={() => { navigate('/DependentProfile');}}>Back</button>
+          <Link to="/DependentProfile">
+            <Button variant="contained" size="large" color="primary" >
+              Back
+            </Button>
+          </Link>
         </div>
       </div>
 
@@ -227,5 +234,5 @@ export default function UpdateDependentBioPage({existingProfile}) {
         <p>.</p>
       </div>
     </div>
-        );
+  );
 }
