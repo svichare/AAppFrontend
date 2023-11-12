67da0610269c9604976aa544348117b76d575e88
technologic808 <20627603+technologic808@users.noreply.github.com>
2023-11-12 21:12:27 +0530

    Fix Actions button scrolling

--- src/pages/conversations_pages/Conversation.jsx
+++ src/pages/conversations_pages/Conversation.jsx
@@ -273,7 +273,7 @@ const Conversation = () => {
             <Backdrop open={open} />
             <SpeedDial
                 ariaLabel="SpeedDial tooltip example"
-                sx={{ position: 'absolute', bottom: 16, right: 16 }}
+                sx={{ position: 'fixed', bottom: 16, right: 16 }}
                 icon={<SpeedDialIcon />}
                 onClose={handleClose}
                 onOpen={handleOpen}
