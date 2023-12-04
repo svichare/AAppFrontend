a94c56350c0fb7c685b2fbf689ef6dfa8615466e
technologic808 <20627603+technologic808@users.noreply.github.com>
2023-12-04 06:19:51 +0530

    Add custom hook for data fetching

---
+++ src/pages/conversations_pages/UseThreads.js
@@ -0,0 +1,32 @@
+import useSWR from 'swr'
+import { getThreads } from '../../graphql/queries'
+import { API } from '@aws-amplify/api'
+import { COLLECTIONS } from './ConversationSettings'
+
+
+export default function useThreads(collectionCode) {
+
+    const fetcher = async () => {
+        const findCollectionByCode = (code) => COLLECTIONS.find(collection => collection.code === code)
+        const collection = findCollectionByCode(collectionCode);
+        const response = await API.graphql({
+            query: getThreads,
+            variables: {
+                collection_name: collection.name,
+                key: undefined,
+                value: undefined
+            },
+        });
+        let threads = response.data.getThreads;
+        console.log('Fetch using API : threads : ', threads ? threads.length : 0);
+        return threads;
+    };
+
+    const { data, error, isLoading } = useSWR(`/api/threads/${collectionCode}`, fetcher)
+
+    return {
+        threads: data,
+        isLoading,
+        isError: error
+    }
+}
